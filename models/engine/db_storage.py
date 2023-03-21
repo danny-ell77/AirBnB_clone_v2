697f34b2f692a1a45a1700a79504674af435bc31
Daniel Olah <danielolahskybrow77@gmail.com>
2023-03-21 20:49:40 +0000

    some changes

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -1,6 +1,6 @@
 import os
 
-from sqlalchemy import create_engine, URL
+from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, scoped_session
 
 from models.amenity import Amenity
