8672890adc37539c2c9f571ca35e01492f8c991a
Daniel Olah <danielolahskybrow77@gmail.com>
2023-04-24 01:35:35 +0000

    added strict_slashes=False for display_c route in #2

--- web_flask/2-c_route.py
+++ web_flask/2-c_route.py
@@ -24,7 +24,7 @@ def hbnb():
 
 
 @app.route("/c/<text>")
-def display_c(text):
+def display_c(text, strict_slashes=False):
     """Displays 'C' followed by the value of <text>
     Replaces any underscores in <text> with slashes.
     """
@@ -33,3 +33,4 @@ def display_c(text):
 
 if __name__ == "__main__":
     app.run("0.0.0.0", port=5000)
+
