4386aad3fca1367264d89feb6df7ffa0d790c929
Daniel Olah <danielolahskybrow77@gmail.com>
2023-04-24 01:27:20 +0000

    added documentation to project

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,9 +1,16 @@
+#!/usr/bin/python3
+"""This script runs a simple flask application server
+The application listens on 0.0.0.0, port 5000.
+Routes:
+    /: Displays 'Hello HBNB!'.
+"""
 from flask import Flask
 
 app = Flask(__name__)
 
 
 @app.route("/", strict_slashes=False)
+"""Displays 'Hello HBNB!'"""
 def hello():
     return "Hello HBNB!"
 
--- web_flask/2-c_route.py
+++ web_flask/2-c_route.py
@@ -1,3 +1,11 @@
+#!/usr/bin/python3
+"""Starts a Flask web application.
+The application listens on 0.0.0.0, port 5000.
+Routes:
+    /: Displays 'Hello HBNB!'.
+    /hbnb: Displays 'HBNB'.
+    /c/<text>: Displays 'C' followed by the value of <text>.
+"""
 from flask import Flask
 
 app = Flask(__name__)
@@ -5,16 +13,21 @@ app = Flask(__name__)
 
 @app.route("/", strict_slashes=False)
 def hello():
+    """Displays 'Hello HBNB!'"""
     return "Hello HBNB!"
 
 
 @app.route("/hbnb", strict_slashes=False)
 def hbnb():
+    """Displays 'HBNB'"""	
     return "HBNB"
 
 
 @app.route("/c/<text>")
 def display_c(text):
+    """Displays 'C' followed by the value of <text>
+    Replaces any underscores in <text> with slashes.
+    """
     return "C {}".format(text.replace("_", " "))
 
 
