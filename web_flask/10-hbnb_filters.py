f6b999ef78add5b7ae635da16719e370e4104640
Olah Daniel <danielolahskybrow77@gmail.com>
2023-04-27 14:39:33 +0100

    setup for hbnb_filters

--- web_flask/10-hbnb_filters.py
+++ web_flask/10-hbnb_filters.py
@@ -9,18 +9,23 @@ from flask import Flask
 from flask import render_template
 
 from models import storage
-from models.state import State
+from models import State, Amenity
 
 app = Flask(__name__)
-app.jinja_env.trim_blocks = True
-app.jinja_env.lstrip_blocks = True
 
 
-@app.route("/states", strict_slashes=False)
-def get_states():
+@app.route("/hbnb_filters", strict_slashes=False)
+def hbnb_filters():
     """Displays 'Hello HBNB!'"""
     states = storage.all(State)
-    return render_template("8-cities_by_states.html", states=states)
+    amenities = storage.all(Amenity)
+    return render_template("10-hbnb_filters.html", states=states, amenities=amenities)
+
+
+@app.teardown_appcontext
+def close_database_connection(exception):
+    """This function closes the db session when the application context ends"""
+    storage.close()
 
 
 if __name__ == "__main__":
